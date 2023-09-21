001ee6db862b58c9abd03070bca0b359eea0ccf6
Stanislav <stasbugaev2006@gmail.com>
2023-09-21 21:20:05 +0500

    init

--- service/productService.js
+++ service/productService.js
@@ -3,7 +3,7 @@ const {createInvoiceLink} = require("../telegram");
 
 class ProductService {
   async getProducts() {
-    const {rows : products} = await db.query('SELECT p.id,p.title,p.image,p.description,p.price,p.alcohol_percent,p.liter,p.bitterness,p.country,p.brewery_name,p.style_name,c.category_title as category, c.class_title as class FROM product p JOIN category c ON  p.category = c.id');
+    const {rows : products} = await db.query('SELECT p.id,p.title,p.image,p.description,p.price,p.alcohol_percent,p.liter,p.bitterness,p.country,p.brewery_name,p.style_name,c.category_title as category, c.class_title as class FROM product p JOIN category c ON  p.category_id = c.id');
     const {rows : categories} = await db.query('SELECT * FROM category');
     return {products, categories}
   }
