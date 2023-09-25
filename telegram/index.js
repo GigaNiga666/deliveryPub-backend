b9a3a7c4a936da5559f4075f4ea1d3a22dde82c6
Stanislav <stasbugaev2006@gmail.com>
2023-09-25 13:48:46 +0500

    init

--- telegram/index.js
+++ telegram/index.js
@@ -1,24 +1,50 @@
 const {Telegraf, session, Markup, Telegram} = require('telegraf')
+const {Stage, BaseScene} = require('telegraf/scenes')
+
+const support = new BaseScene('support')
+support.on('message', ctx =>  {
+  ctx.forwardMessage(process.env.SUPPORT_GROUP, ctx.message.chat.id, ctx.message.message_id)
+  return ctx.scene.leave()
+})
+support.action('end', async (ctx) => {
+  await telegram.editMessageText(ctx.chat.id, ctx.update.callback_query.message.message_id, undefined, ctx.update.callback_query.message.text, Markup.inlineKeyboard([]))
+  return ctx.scene.leave()
+})
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const telegram = new Telegram(process.env.BOT_TOKEN)
+const stage = new Stage([support])
 
 bot.use(session())
+bot.use(stage.middleware())
 
 bot.start((ctx) => {
   ctx.reply(`Выберете действие:`, Markup.inlineKeyboard([
     Markup.button.webApp('Сайт', `${process.env.CLIENT_URL}`),
-  ]))
-  ctx.reply('Тех-поддержка', Markup.keyboard([
-    ['лааллалала'],
-    ['Техническая поддержка']
+    Markup.button.callback('Тех. поддержка', 'supportEnter')
   ]))
 })
-bot.hears('Техническая поддержка', ctx => {
-  console.log('ID CHAT',ctx.message.chat.id)
-  console.log('ID MESSAGE',ctx.message.message_id)
-  ctx.forwardMessage(process.env.SUPPORT_GROUP, ctx.message.chat.id, ctx.message.message_id)
+
+bot.action('supportEnter', async (ctx) => {
+  await ctx.reply('Задайте интирисующий вас вопрос')
+  return ctx.scene.enter('support')
 })
+
+bot.action(/supportRes/, async (ctx) => {
+  await telegram.editMessageText(ctx.chat.id, ctx.update.callback_query.message.message_id, undefined, ctx.match.input.replace(/^.{11}/, ''), Markup.inlineKeyboard([
+    Markup.button.callback('Завершить сеанс', 'end')
+  ]))
+  return ctx.scene.enter('support')
+})
+
+bot.on('message', ctx => {
+  if (ctx.chat.id === +process.env.SUPPORT_GROUP && ctx.message.reply_to_message) {
+    return telegram.sendMessage(ctx.message.reply_to_message.forward_from.id, '✉ Ответ от тех. поддержки:', Markup.inlineKeyboard([
+      Markup.button.callback('Прочитать', `supportRes-${ctx.message.text}`),
+    ]))
+  }
+})
+
 const launchBot = () => bot.launch();
 
 const answerWebAppQuery = async (data) => {
