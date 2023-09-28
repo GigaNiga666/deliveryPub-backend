e4cb6684ecdc7a5fce35499a976885584eef3b27
Stanislav <stasbugaev2006@gmail.com>
2023-09-29 00:15:21 +0500

    init

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const upload = require('express-fileupload')
 const cors = require("cors");
+const https = require('https');
+const fs = require('fs');
 const router = require("./router");
 require('./telegram/index').launchBot()
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+const options = {
+  cert: fs.readFileSync(process.env.PATH_TO_CERT),
+  key: fs.readFileSync(process.env.PATH_TO_KEY)
+};
 
 app.use(express.json())
 app.use(upload({
@@ -19,5 +26,14 @@ app.use(cors({
 app.use('/api', router)
 app.use('/client/public', express.static('./client/public'))
 
-app.listen(PORT, () => console.log('Server start on PORT: ' + PORT))
+const server = https.createServer(options, app);
+
+server.listen(8001, function(){
+  console.log('Server start on PORT: ' + PORT)
+});
+
+
+
+
+
 
