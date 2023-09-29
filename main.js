12a0b9841c6a9cbbe086c43899befb460fe6a132
Stanislav <stasbugaev2006@gmail.com>
2023-09-29 18:12:38 +0500

    init

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 const options = {
-  cert: fs.readFileSync(`${process.env.PATH_TO_CERT}`),
-  key: fs.readFileSync(`${process.env.PATH_TO_KEY}`)
+  cert: fs.readFileSync(__dirname + `/${process.env.PATH_TO_CERT}`),
+  key: fs.readFileSync(__dirname + `/${process.env.PATH_TO_KEY}`)
 };
 
 app.use(express.json())
