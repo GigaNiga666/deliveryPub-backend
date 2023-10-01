a8580c381201261e44fb5bec561e03f86d0dc824
Stanislav <stasbugaev2006@gmail.com>
2023-10-01 10:26:45 +0500

    init

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('events').EventEmitter.prototype._maxListeners = 100;
 const express = require('express')
 const upload = require('express-fileupload')
 const cors = require("cors");
